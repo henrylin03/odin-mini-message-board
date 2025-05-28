2f421e2d2dab00790ef4f6397804ded7de500331
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-05-29 08:28:37 +1000

    batman

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mini Message Board
+Building a mini message board using ExpressJS, with templating in EJS. 
+
+This is part of The Odin Project's NodeJS Course.
---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require("node:path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+/* set up EJS templating engine */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
