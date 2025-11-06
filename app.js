4a8a1a887331e73712c16304c1b0c9932435b37f
Henry <83106787+henrylin03@users.noreply.github.com>
2025-11-06 12:30:58 +1100

    chore: set up biome and format ejs files (#3)
    * chore: format ejs per html
    
    this was done in settings.json for vscode per https://stackoverflow.com/questions/45069089/ejs-formatting-in-vs-code
    
    * chore: install biome. remove prettier. apply biome to all files

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use("/messages", messageRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}!`);
+	console.log(`Listening on port ${PORT}!`);
 });
--- controllers/messageController.js
+++ controllers/messageController.js
@@ -2,19 +2,19 @@ const db = require("../db");
 const CustomNotFoundError = require("../errors/CustomNotFoundError");
 
 async function getMessageById(req, res) {
-  const { messageId } = req.params;
-  const message = await db.getMessageById(messageId);
+	const { messageId } = req.params;
+	const message = await db.getMessageById(messageId);
 
-  if (!message) {
-    throw new CustomNotFoundError("Message not found");
-  }
+	if (!message) {
+		throw new CustomNotFoundError("Message not found");
+	}
 
-  res.render("message", {
-    messageId: messageId,
-    text: message.text,
-    user: message.user,
-    added: message.added,
-  });
+	res.render("message", {
+		messageId: messageId,
+		text: message.text,
+		user: message.user,
+		added: message.added,
+	});
 }
 
 module.exports = { getMessageById };
