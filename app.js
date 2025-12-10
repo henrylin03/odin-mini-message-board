69a8a1ab8385ff12b2b63507852848c319cae1a0
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-11 09:10:54 +1100

    refactor(app): add error handling when listening. change order ot .set on app to be more intuitive imo

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
 /* set up EJS templating engine */
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 /* routes */
 app.use("/", indexRouter);
 app.use("/messages", messageRouter);
 
 const PORT = process.env.APP_PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+	if (error) throw error;
 	console.log(`Listening on port ${PORT}!`);
 });
