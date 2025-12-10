db34ab146b4fc0b73dda7dae370789bbd3883c34
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-10 16:49:57 +1100

    refactor(pool): use connection URI to defining connection information
    this is so that I can actually use the same environment variable in my populatedb script

--- db/pool.js
+++ db/pool.js
@@ -2,15 +2,8 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
-const PORT = 5432;
-
 module.exports = new Pool({
-	host: PGHOST,
-	database: PGDATABASE,
-	username: PGUSER,
-	password: PGPASSWORD,
-	port: PORT,
+	connectionString: process.env.PG_DATABASE_URL,
 	ssl: {
 		require: true,
 	},
