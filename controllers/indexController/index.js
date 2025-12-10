f295a92abd57f91c517ce58949f980a1f84641cf
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-12-11 09:31:23 +1100

    feat(indexController): validate message text and username based on character requirements
    this is in separate validateMessage.js in indexController. this is then called within the indexController. I've also refactored the indexController methods to be arrow functions because I didn't really know how to keep it asynchronous while passing in validateMessage :\

---
+++ controllers/indexController/index.js
@@ -0,0 +1,26 @@
+const { validationResult } = require("express-validator");
+const db = require("../../db/queries");
+const { validateMessage } = require("./validateMessage");
+
+const getAllMessages = async (_req, res) => {
+	const messages = await db.getAllMessages();
+	res.render("index", { title: "Mini Messageboard", messages: messages });
+};
+
+const postNewMessage = [
+	validateMessage,
+	async (req, res) => {
+		const inputErrors = validationResult(req);
+		console.log("inputErrors:", inputErrors);
+		if (!inputErrors.isEmpty())
+			return res.status(400).render("form", {
+				inputErrors: inputErrors.array(),
+			});
+
+		const { text, username } = req.body;
+		await db.addNewMessage({ text, username });
+		res.redirect("/");
+	},
+];
+
+module.exports = { getAllMessages, postNewMessage };
